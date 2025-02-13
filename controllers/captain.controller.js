80caa7c64e049a79cb8f90e108a62e56e1432a95
Siddhant <sidhantsrivastav13@gmail.com>
2025-02-13 15:12:29 +0530

    Resolve merge conflicts and update documentation

--- controllers/captain.controller.js
+++ controllers/captain.controller.js
@@ -57,3 +57,16 @@ module.exports.loginCaptain = async (req, res, next) => {
     res.cookie('token',token);
     res.status(200).json({token,captain});
 }
+
+module.exports.getCaptainProfile=async(req,res,next)=>{
+    res.status(200).json(req.captain);
+}
+
+module.exports.logoutCaptain=async(req,res,next)=>{
+  const token=req.cookies.token || req.headers.authorization?.split(' ')[1];
+    
+  await blackListTokenModel.create({token});  
+  
+  res.clearCookie('token');
+    res.status(200).json({message:'Logged out successfully'});
+}
